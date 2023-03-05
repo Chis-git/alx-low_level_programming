04c1c82b891744919eebd44621ba8a0baebc6229
Chis-git <christophernkomo5@gmail.com>
2023-03-05 05:18:03 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,19 +2,22 @@
 
 
 /**
- * print_most_numbers - print num except 2 and 4
- * return: void
+ * print_most_numbers - print the numbers since 0 up to 9
+ * Description: prints the numbers excluding 2 and 4
+ * Return: The numbers since 0 up to 9
  */
-
 
 void print_most_numbers(void)
 {
-	char c;
+	int x = 0;
 
-	for (c = '0'; c <= '9'; c++)
+	for (; x <= 9; x++)
 	{
-	if (!(c == '2' || c == '4'))
-	_putchar(c);
+	continue;
+	}
+	else
+	{
+	_putchar(x + '0');
 	}
 	_putchar('\n');
 }
