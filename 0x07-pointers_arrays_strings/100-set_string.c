8d1676be97ae1d07e647d4b90f34fe9c9611c08b
Chis-git <christophernkomo5@gmail.com>
2023-03-07 04:24:56 -0800

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - set the value of a pointerto a char
+ * @s: pointer to pointer
+ * @to: pointer char
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
