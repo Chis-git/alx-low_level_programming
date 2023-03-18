b7cd9264312b355261ba00223f5ab6a39c608b17
Chis-git <christophernkomo5@gmail.com>
2023-03-17 23:01:52 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,21 +2,44 @@
 #include <stdlib.h>
 
 /**
- * array_range - creates an array of integers
- * @min: minimum
- * @max: maximum
- * Return: array
+ * alloc_grid - ...
+ * @width:  ...
+ * @height: ...
+ *
+ * Return: ...
  */
-int *array_range(int min, int max)
+int **alloc_grid(int width, int height)
 {
-	int arr, i = 0, t = min;
+	int i, j, k, l;
+	int **a;
 
-	if (min > max)
-		return (0);
-	arr = malloc((max - min + 1) * sizeof(int));
-	if (!arr)
-		return (0);
-	while (i <= max - min)
-		arr[i++] = t++;
-	return (arr);
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	a = malloc(sizeof(int *) * height);
+		if (a == NULL)
+		{
+			free(a);
+			return (NULL);
+		}
+	for (i = 0; i < height; i++)
+	{
+		a[i] = malloc(sizeof(int) * width);
+		if (a[i] == NULL)
+		{
+			for (j = i; j >= 0; j--)
+			{
+				free(a[j]);
+			}
+			free(a);
+			return (NULL);
+		}
+	}
+	for (k = 0; k < height; k++)
+	{
+		for (l = 0; l < width; l++)
+		{
+			a[k][l] = 0;
+		}
+	}
+	return (a);
 }
